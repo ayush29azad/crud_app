d9234914d10fd8877fde11dd91832f0c312b2e42
Ayush Azad <ayush29azad@gmail.com>
2021-03-13 20:02:22 +0530

    header+footer added

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ const dotenv = require('dotenv');
 const app = express();
 const morgan =require('morgan');
 const path = require('path');
+dotenv.config({path:'./config.env'});
 const PORT = process.env.PORT ||8080
-dotenv.config({path:'config.env'});
 
-app.get('/',(req,res)=>{
-res.send("Crud Application");
-});
+
 // parse request to body parser
 
 app.use(express.urlencoded({ extended: true }));
@@ -19,17 +17,29 @@ app.use(morgan('tiny'));
 
 // set view engine 
 app.set("view engine","ejs");
-app.set("views",path.resolve(__dirname,"views/ejs"))
+app.set("views",path.join(__dirname,"views"))
 //load assets
 app.use('/css',express.static(path.resolve(__dirname,"assets/css")));
 //css/style.css
 app.use('/css',express.static(path.resolve(__dirname,"assets/img")));
 app.use('/css',express.static(path.resolve(__dirname,"assets/js")));
 
-app.listen(PORT,()=>{
 
-console.log('Server is running on http://localhost:${PORT}');
+app.get('/', (req,res)=>{
 
+    res.render('index');
+    
 });
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT,()=>{console.log('Server is running Sucessfully ');});
 
 
